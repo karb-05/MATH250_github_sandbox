e975de3b37e5e8f3520c868e3751da9ee5d15f9e
Karli <kgb777123jr@gmail.com>
2025-11-03 14:16:57 -0600

    added push error & ghost image

--- github- retry.R
+++ github- retry.R
@@ -3,11 +3,10 @@
 
 library(usethis)
 #first register for github
-create_github_token() #90 days for name: MATH250-GitHub sandbox 2
+create_github_token() #90 days MATH250-github sandbox
 gitcreds::gitcreds_set()
-
 
 use_github()
 
 
-#make a change or two --> add ghost cartoon to sandbox.
+#make a change or two --> add ghost cartoon to sandbox
