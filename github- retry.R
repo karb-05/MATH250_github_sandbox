422fa43b75d175edf5d9856eddb3cdd4bfed4f26
Karli <kgb777123jr@gmail.com>
2025-11-03 14:14:11 -0600

    initial commit

---
+++ github- retry.R
@@ -0,0 +1,13 @@
+'github set up retry'
+
+
+library(usethis)
+#first register for github
+create_github_token() #90 days for name: MATH250-GitHub sandbox 2
+gitcreds::gitcreds_set()
+
+
+use_github()
+
+
+#make a change or two --> add ghost cartoon to sandbox.
